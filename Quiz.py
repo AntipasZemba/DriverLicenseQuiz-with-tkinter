776b20dcbc0717a331ee2eebf5286bce4d14f3a8
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2025-05-23 16:40:59 +0000

    update

--- Quiz.py
+++ Quiz.py
@@ -12,3 +12,15 @@ class User:
         self.id_number = id_number
         self.age = age
 
+class Question:
+    def __init__(self, prompt_en, prompt_fr, options_en, options_fr, correct_option, category):
+        self.prompt_en = prompt_en
+        self.prompt_fr = prompt_fr
+        self.options_en = options_en
+        self.options_fr = options_fr
+        self.correct_option = correct_option.upper()
+        self.category = category
+
+    def is_correct(self, answer):
+        return answer.upper() == self.correct_option
+
