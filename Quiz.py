94a9844bca2089f930705182fe7e2071e5d8ddc8
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2025-05-23 16:40:04 +0000

    update

--- Quiz.py
+++ Quiz.py
@@ -4,3 +4,11 @@ from tkinter import ttk
 import time
 import random
 
+# --- User & Question Classes ---
+class User:
+    def __init__(self, first_name, last_name, id_number, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
+        self.age = age
+
